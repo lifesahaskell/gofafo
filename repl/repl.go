67d5fe556c989c9fa022d1b23fe9107857aa2292
Corey Todalen <todal001@d.umn.edu>
2024-02-13 15:47:58 -0600

    Added evaluation for functions.

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fafolang/evaluator"
 	"fafolang/lexer"
+	"fafolang/object"
 	"fafolang/parser"
 	"fmt"
 	"io"
@@ -13,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
